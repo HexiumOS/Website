4b36b47c74cc83623cc13bb13a8ab459cc9a8761
Viktor Popp <viktorpopp.dev@gmail.com>
2025-04-07 17:04:16 +0200

    Removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,6 @@
 import type {ReactNode} from 'react';
-import clsx from 'clsx';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
 
 export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
